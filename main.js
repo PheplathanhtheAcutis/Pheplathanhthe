50a7bd1227a056a53267d7de2fa643a4085cf6a4
PheplathanhtheAcutis <tungbiry2904@gmail.com>
2025-09-18 10:04:28 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Phép Lạ Thánh Thể – Carlo Acutis đã khởi chạy");
+  console.log("Trang web Phép Lạ Thánh Thể đã sẵn sàng!");
 });
